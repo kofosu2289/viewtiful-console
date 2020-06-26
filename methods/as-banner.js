430fe9450443d03879d84615ada0e3f90f273005
Kenneth Ofosu <kofosu89@gmail.com>
2020-06-26 13:53:50 -0400

    add ability to manually split rows for asBanner

--- methods/as-banner.js
+++ methods/as-banner.js
@@ -1,14 +1,20 @@
+/* eslint-disable no-restricted-syntax */
 const termSize = require("term-size");
 const wordWrap = require("wordwrap");
 const colors = require("colors");
 const boxen = require("boxen");
 
-const asBanner = (str) => {
-  const rowWidth = termSize().columns;
-  const rows = wordWrap(rowWidth - 10)(str);
-  const strWidth = Math.min(str.length, rowWidth - 10);
-  const horizontalPadding = Math.round((rowWidth - strWidth) / 2) - 5;
-
+const asBanner = (...strings) => {
+  const termWidth = termSize().columns;
+  const rows = Array.from(strings).map(str => wordWrap(termWidth - 10)(str.toUpperCase()));
+  let greatestLength = 0;
+  for (const row of rows) {
+    for (const subStr of row.split("\n")) {
+      greatestLength = Math.max(greatestLength, subStr.length);
+    }
+  }
+  const strWidth = Math.min(greatestLength, termWidth - 10);
+  const horizontalPadding = Math.round((termWidth - strWidth) / 2) - 5;
   const options = {
     borderColor: "magenta",
     float: "center",
@@ -22,7 +28,7 @@ const asBanner = (str) => {
     },
   };
 
-  console.log(colors.cyan(boxen(rows.toUpperCase(), options)));
+  console.log(colors.cyan(boxen(rows.join('\n'), options)));
 };
 
 module.exports = asBanner;
