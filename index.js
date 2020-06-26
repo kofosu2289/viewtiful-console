0feb5224ff2f6d91bd040700264f5832714cc982
Kenneth Ofosu <kofosu89@gmail.com>
2020-06-26 14:47:02 -0400

    re-add label as an alias for the new withLabel

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const withLabel = require("./methods/with-label.js");
+const label = require('./methods/with-label.js');
 const atTop = require("./methods/at-top.js");
 const asBanner = require("./methods/as-banner.js");
 const log = require("./methods/log.js");
 
 const viewtifulConsole = {
+  label,
   withLabel,
   atTop,
   asBanner,
