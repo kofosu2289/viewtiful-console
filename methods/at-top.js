c603eff1847210c0315b187074fbc49bde233167
Kenneth Ofosu <kofosu89@gmail.com>
2020-06-26 01:20:59 -0400

    finish refactor

---
+++ methods/at-top.js
@@ -0,0 +1,8 @@
+const colors = require("colors");
+
+const atTop = (str) => {
+  console.clear();
+  console.log(str);
+};
+
+module.exports = atTop;
---
+++ methods/with-label.js
@@ -0,0 +1,12 @@
+const colors = require("colors");
+
+const withLabel = (variables) => {
+  Object.keys(variables).forEach((variableName) => {
+    const value = variables[variableName];
+    console.log(colors.magenta(variableName));
+    console.log(colors.magenta.bold("-> "), colors.cyan(value));
+    console.log("");
+  });
+};
+
+module.exports = withLabel;
